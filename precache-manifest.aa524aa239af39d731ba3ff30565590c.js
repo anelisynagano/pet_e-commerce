87c55cec5f02ba27fdfbc97bf8a20655c0d482fe
WildCodeSchool <anelisynagano@gmail.com>
2020-08-21 15:12:25 +0100

    Updates

---
+++ precache-manifest.aa524aa239af39d731ba3ff30565590c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680a3dc606628832af7ec5d9d681923b",
+    "url": "/pet_e-commerce/index.html"
+  },
+  {
+    "revision": "63ddb1b6c167e71d2ce5",
+    "url": "/pet_e-commerce/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "663c42f7d848325c8b09",
+    "url": "/pet_e-commerce/static/css/main.85ff2b7f.chunk.css"
+  },
+  {
+    "revision": "63ddb1b6c167e71d2ce5",
+    "url": "/pet_e-commerce/static/js/2.f0bb36a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pet_e-commerce/static/js/2.f0bb36a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663c42f7d848325c8b09",
+    "url": "/pet_e-commerce/static/js/main.b1ed0887.chunk.js"
+  },
+  {
+    "revision": "922e98ac27d3efb7bd37",
+    "url": "/pet_e-commerce/static/js/runtime-main.520d3582.js"
+  }
+]);
